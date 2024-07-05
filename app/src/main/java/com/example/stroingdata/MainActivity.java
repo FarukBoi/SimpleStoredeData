a230b8ea185281fdd958c8f03b83bb66016f1d8d
Ömer Faruk Kaba <farukkaba4807@gmail.com>
2024-07-05 19:00:11 +0300

    Alert Dialog added

--- app/src/main/java/com/example/stroingdata/MainActivity.java
+++ app/src/main/java/com/example/stroingdata/MainActivity.java
@@ -1,14 +1,18 @@
 package com.example.stroingdata;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -19,6 +23,7 @@ public class MainActivity extends AppCompatActivity {
     TextView resultText;
     SharedPreferences sharedPreferences;
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -42,14 +47,35 @@ public class MainActivity extends AppCompatActivity {
         }else {
             resultText.setText("Stored age: "+storedAge);
         }
+
+        Toast.makeText(MainActivity.this,"Toast Message",Toast.LENGTH_LONG).show();
     }
 
     //save a stored data for restarting
     public void save(View view){
+        AlertDialog.Builder alert = new AlertDialog.Builder(this);
+
+        alert.setTitle("Save");
+        alert.setMessage("Are you sure?");
         if(!editTextNumber.getText().toString().matches("")){
-            int userAge = Integer.parseInt(editTextNumber.getText().toString());
-            resultText.setText("Your age: "+userAge);
-            sharedPreferences.edit().putInt("storedAge",userAge).apply(); //set stored data (putInt("key",assignedValue))
+            alert.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    int userAge = Integer.parseInt(editTextNumber.getText().toString());
+                    resultText.setText("Your age: "+userAge);
+                    sharedPreferences.edit().putInt("storedAge",userAge).apply(); //set stored data (putInt("key",assignedValue))
+                    Toast.makeText(MainActivity.this,"Saved",Toast.LENGTH_LONG).show();
+                }
+            });
+            
+            alert.setNegativeButton("No", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    Toast.makeText(MainActivity.this,"Not Saved",Toast.LENGTH_LONG).show();
+                }
+            });
+            alert.show();
+
         }else {
             resultText.setText("Enter your age!");
         }
@@ -58,11 +84,12 @@ public class MainActivity extends AppCompatActivity {
     //remove stored data
     public void delete(View view){
         int storedData = sharedPreferences.getInt("storedAge",0); //get stored data(getInt("key",defaultValue))
-            if (storedData != 0){
-                sharedPreferences.edit().remove("storedAge").apply();
-                resultText.setText("Your age:");
-            }
+        if (storedData != 0){
+            sharedPreferences.edit().remove("storedAge").apply();
+            resultText.setText("Your age:");
         }
+        Toast.makeText(MainActivity.this,"Your save deleted",Toast.LENGTH_LONG).show();
     }
+}
 
 
